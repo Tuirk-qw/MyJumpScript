cb854e0db4357548da9e17ca7fb5ec13ea252945
Tuirk-qw <rxzmlk22@gmail.com>
2026-03-05 13:05:02 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local Player = game.Players.LocalPlayer
 UIS.JumpRequest:Connect(function()
     local Character = Player.Character
     if Character and Character:FindFirstChild("HumanoidRootPart") then
-        -- دفع اللاعب للأعلى يدوياً بدلاً من طلب قفزة رسمية
+        -- يمكنك تغيير الرقم 50 لزيادة قوة القفزة
         Character.HumanoidRootPart.Velocity = Vector3.new(0, 50, 0)
     end
 end)
